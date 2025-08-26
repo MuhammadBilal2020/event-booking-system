081d463688a26b6853a630f083362410e2509829
MuhammadBilal2020 <smit.11.aandw@gmail.com>
2025-08-26 11:24:13 +0500

    rechange

---
+++ middlewares.js
@@ -0,0 +1,37 @@
+// // middleware.js
+// import { NextResponse } from "next/server";
+// // import jwt from "jsonwebtoken";
+// import LoginPage from "./app/frontend/admin/login/page";
+// import { checkAccessToken } from "./app/utils/checkAcecessToken.js";
+
+// export function middleware(req) {
+//     const url = req.nextUrl.clone();
+//     const { pathname } = url;
+
+//     // Public routes jahan redirect nahi karna
+//     if (
+//         pathname.startsWith("/api/user/auth/login") || // API login ko bhi exclude karen
+//         pathname.startsWith("/api/user/auth/refresh") || // API refresh ko bhi exclude karen
+//         pathname.startsWith("/frontend/publicUser/loginUser") ||
+//         pathname.startsWith("/frontend/admin/login")
+//     ) {
+//         return NextResponse.next();
+//     }
+
+//     const accessToken = req.cookies.get("accessToken")?.value;
+//     if (!accessToken) {
+//         url.pathname = "/frontend/publicUser/loginUser";
+//         return NextResponse.redirect(url);
+//     }
+
+
+//    checkAccessToken(accessToken)
+
+// }
+
+
+
+// export const config = {
+//     matcher: ["/frontend:path*"],
+// };
+
